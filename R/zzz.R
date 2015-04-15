a0626b6ab6c3bf5fd03c3cd81e64a3a3fd45877c
John Hughes <hughesj@umn.edu>
2015-04-15 00:00:00 +0000

    version 1.0-1

--- R/zzz.R
+++ R/zzz.R
@@ -8,7 +8,7 @@
     temp = packageDescription("copCAR")
     msg = paste(temp$Package, ": ", temp$Title, "\n", "Version ", temp$Version,
                 " created on ", temp$Date, ".\n", sep = "")
-    msg = paste(msg, "copyright (c) 2014, John Hughes, University of Minnesota\n",
+    msg = paste(msg, "copyright (c) 2014-15, John Hughes, University of Minnesota\n",
                 sep = "")
     msg = paste(msg, 'For citation information, type citation("copCAR").\n', sep = "")
     msg = paste(msg, 'Type help(package = copCAR) to get started.\n', sep = "")
